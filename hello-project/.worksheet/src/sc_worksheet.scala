0cbdd79444b68166afe42fadcc0dcc67c9e40742
kulinskyvs <kulinskyvs@gmail.com>
2013-03-31 23:52:43 +0300

    Creating first project in scala

---
+++ hello-project/.worksheet/src/sc_worksheet.scala
@@ -0,0 +1,3 @@
+object sc_worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(65); 
+  println("Welcome to the Scala worksheet")}
+}
