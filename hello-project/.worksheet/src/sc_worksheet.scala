89c81715d5fa4209a0fd23ad9f13722852afa6b2
kulinskyvs <kulinskyvs@gmail.com>
2013-03-31 23:56:04 +0300

    Revert "Creating first project in scala"
    This reverts commit 0cbdd79444b68166afe42fadcc0dcc67c9e40742.

--- hello-project/.worksheet/src/sc_worksheet.scala
+++
@@ -1,3 +0,0 @@
-object sc_worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(65); 
-  println("Welcome to the Scala worksheet")}
-}
